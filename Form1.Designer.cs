662cc1362db7f5b9191202388970c814aa112a45
d13b <118538911+d13b@users.noreply.github.com>
2024-12-13 12:59:53 +0000

    Changed overall layout for better UI, added text boxes for easy task input.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,52 +32,123 @@
             this.btnAddTask = new System.Windows.Forms.Button();
             this.btnEditTask = new System.Windows.Forms.Button();
             this.btnRemoveTask = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.pnlTitleBack = new System.Windows.Forms.Panel();
+            this.lblTaskTitle = new System.Windows.Forms.Label();
+            this.txtTaskTitle = new System.Windows.Forms.TextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtDescription = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvTasks)).BeginInit();
+            this.pnlTitleBack.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvTasks
             // 
             this.dgvTasks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvTasks.Location = new System.Drawing.Point(12, 12);
+            this.dgvTasks.Location = new System.Drawing.Point(12, 217);
             this.dgvTasks.Name = "dgvTasks";
-            this.dgvTasks.Size = new System.Drawing.Size(776, 344);
+            this.dgvTasks.Size = new System.Drawing.Size(776, 299);
             this.dgvTasks.TabIndex = 0;
             // 
             // btnAddTask
             // 
-            this.btnAddTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAddTask.Location = new System.Drawing.Point(12, 380);
+            this.btnAddTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddTask.Location = new System.Drawing.Point(12, 183);
             this.btnAddTask.Name = "btnAddTask";
-            this.btnAddTask.Size = new System.Drawing.Size(103, 48);
+            this.btnAddTask.Size = new System.Drawing.Size(250, 28);
             this.btnAddTask.TabIndex = 1;
-            this.btnAddTask.Text = "Add Task";
+            this.btnAddTask.Text = "Add";
             this.btnAddTask.UseVisualStyleBackColor = true;
             // 
             // btnEditTask
             // 
-            this.btnEditTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEditTask.Location = new System.Drawing.Point(342, 380);
+            this.btnEditTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnEditTask.Location = new System.Drawing.Point(275, 183);
             this.btnEditTask.Name = "btnEditTask";
-            this.btnEditTask.Size = new System.Drawing.Size(103, 48);
+            this.btnEditTask.Size = new System.Drawing.Size(250, 28);
             this.btnEditTask.TabIndex = 2;
-            this.btnEditTask.Text = "Edit Task";
+            this.btnEditTask.Text = "Edit";
             this.btnEditTask.UseVisualStyleBackColor = true;
             // 
             // btnRemoveTask
             // 
-            this.btnRemoveTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRemoveTask.Location = new System.Drawing.Point(685, 381);
+            this.btnRemoveTask.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold);
+            this.btnRemoveTask.Location = new System.Drawing.Point(538, 183);
             this.btnRemoveTask.Name = "btnRemoveTask";
-            this.btnRemoveTask.Size = new System.Drawing.Size(103, 48);
+            this.btnRemoveTask.Size = new System.Drawing.Size(250, 28);
             this.btnRemoveTask.TabIndex = 3;
-            this.btnRemoveTask.Text = "Remove Task";
+            this.btnRemoveTask.Text = "Remove";
             this.btnRemoveTask.UseVisualStyleBackColor = true;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(802, 85);
+            this.lblTitle.TabIndex = 4;
+            this.lblTitle.Text = "The Task Manager";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlTitleBack
+            // 
+            this.pnlTitleBack.BackColor = System.Drawing.Color.Olive;
+            this.pnlTitleBack.Controls.Add(this.lblTitle);
+            this.pnlTitleBack.Location = new System.Drawing.Point(0, -1);
+            this.pnlTitleBack.Name = "pnlTitleBack";
+            this.pnlTitleBack.Size = new System.Drawing.Size(802, 85);
+            this.pnlTitleBack.TabIndex = 5;
+            // 
+            // lblTaskTitle
+            // 
+            this.lblTaskTitle.AutoSize = true;
+            this.lblTaskTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTaskTitle.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblTaskTitle.Location = new System.Drawing.Point(366, 87);
+            this.lblTaskTitle.Name = "lblTaskTitle";
+            this.lblTaskTitle.Size = new System.Drawing.Size(67, 16);
+            this.lblTaskTitle.TabIndex = 6;
+            this.lblTaskTitle.Text = "Task Title";
+            // 
+            // txtTaskTitle
+            // 
+            this.txtTaskTitle.Location = new System.Drawing.Point(12, 107);
+            this.txtTaskTitle.Name = "txtTaskTitle";
+            this.txtTaskTitle.Size = new System.Drawing.Size(776, 20);
+            this.txtTaskTitle.TabIndex = 7;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.lblDescription.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblDescription.Location = new System.Drawing.Point(349, 132);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(109, 16);
+            this.lblDescription.TabIndex = 8;
+            this.lblDescription.Text = "Task Description";
+            this.lblDescription.Click += new System.EventHandler(this.lblDescription_Click);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(13, 151);
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(775, 20);
+            this.txtDescription.TabIndex = 9;
             // 
             // formMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 528);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.txtTaskTitle);
+            this.Controls.Add(this.lblTaskTitle);
+            this.Controls.Add(this.pnlTitleBack);
             this.Controls.Add(this.btnRemoveTask);
             this.Controls.Add(this.btnEditTask);
             this.Controls.Add(this.btnAddTask);
@@ -85,7 +156,9 @@
             this.Name = "formMain";
             this.Text = "Task Manager";
             ((System.ComponentModel.ISupportInitialize)(this.dgvTasks)).EndInit();
+            this.pnlTitleBack.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -95,6 +168,12 @@
         private System.Windows.Forms.Button btnAddTask;
         private System.Windows.Forms.Button btnEditTask;
         private System.Windows.Forms.Button btnRemoveTask;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Panel pnlTitleBack;
+        private System.Windows.Forms.Label lblTaskTitle;
+        private System.Windows.Forms.TextBox txtTaskTitle;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox txtDescription;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Portfolio_TaskManager
         {
             InitializeComponent();
         }
+
+        private void lblDescription_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
